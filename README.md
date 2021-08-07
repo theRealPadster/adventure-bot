7c4cfa71a11f2b39ce6a6dbdadb3a4672fbbea8f
theRealPadster <padspambox@gmail.com>
2021-08-07 18:33:59 -0400

    Interact with bot via mentions, Remove unused files

--- README.md
+++ README.md
@@ -1,21 +1,14 @@
-# Super simple Discord bot written in TypeScript
-_I don't do much yet but I'm learning_
+# Discord bot to play a text-based adventure game
+This choose your own adventure game is based on the sms-based one I wrote here: https://github.com/theRealPadster/buildyourownadventurebot.
 
 ## .env setup
 ```
 CLIENT_TOKEN="yourtoken"
-COMPLIMENT_USERS=["user1","user2"]
 ```
 
-## Commands
-### `/hello`
-Greet the user
-### `/compliment`
-Compliment or insult the user, based on if their username is in the COMPLIMENT_USERS array or not. You can mention a user to pass them instead of yourself. The compliments and insults are specified in `src/data/compliments.json` and `src/data/insults.json`. The user will be mentioned whenever `$1` is used in the strings.
-### `/args-info`
-Output any arguments specified in the message (e.g. `/args-info foo bar` -> "foo", "bar")
-### `/g commands`
-Play choose your own adventure game (from https://github.com/theRealPadster/buildyourownadventurebot). You must prefix any commands to the adventure game with `/g`. e.g. `/g status` or `/g enter kitchen`. I will likely split command into a separate bot. 
+## Playing the game
+To start the game, just mention the bot. To continue playing, mention the bot and then type your command.
+e.g. `@AdventureBot status` or `@AdventureBot enter kitchen`
 
 ## Development
 Run TypeScript watch task and use nodemon to apply changes in real time.
--- src/index.ts
+++ src/index.ts
@@ -2,55 +2,32 @@ import dotenv from 'dotenv';
 dotenv.config();
 
 import Discord from 'discord.js';
-import { getComplimentOrInsult, checkAndReactToGifs, handleAdventure } from './lib/commands';
+
+import AdventureGame from './lib/adventure';
+const adventureGame = new AdventureGame();
 
 const client = new Discord.Client();
 
-const PREFIX = '/';
-
-client.on('ready', () => {
+client.once('ready', () => {
   console.log(`Logged in as ${client.user.tag}!`);
 });
 
 client.on('message', async (msg) => {
 
-  // Sends reply in function, returns true if sent
-  if (checkAndReactToGifs(msg)) return;
+  const BOT_ID = client.user.id;
+  // If user didn't mention the bot, ignore
+  if (!msg.mentions.users.get(BOT_ID) || msg.author.bot) return;
 
-  // Abort if message is not a command for the bot
-  if (!msg.content.startsWith(PREFIX) || msg.author.bot) return;
+  // const command = msg.content.slice(`<@!${BOT_ID}>`.length);
+  const command = msg.content.replace(`<@!${BOT_ID}>`,'').trim();
+  console.log(command);
 
-  const args = msg.content.slice(PREFIX.length).trim().split(' ');
-	const command = args.shift().toLowerCase();
+  // const args = msg.content.slice(`<@!${BOT_ID}>`.length).trim().split(' ');
+	// const command = args.shift().toLowerCase();
+  // console.log(command, args, msg.content);
 
-  // /hello
-  if (command === 'hello') {
-    try {
-      await msg.react('👋');
-      await msg.channel.send(`Hello ${msg.author}! :)`);
-      // await msg.reply(`Hello @${msg.author.username}! :)`);
-
-    } catch (error) {
-      console.error('Something broke:', error)
-    }
-  } // /compliment
-  else if (command === 'compliment') {
-    // Use mentioned user or the author if none
-    const user = msg.mentions.users.first() || msg.author;
-    const response = getComplimentOrInsult(user);
-    msg.channel.send(response);
-  }	// /args-info
-  else if (command === 'args-info') {
-		if (!args.length) {
-			return msg.channel.send(`You didn't provide any arguments, ${msg.author}!`);
-		}
-
-		msg.channel.send(`Command name: ${command}\nArguments: ${args}`);
-	} // choose your own adventure game
-  else if (command === 'g') {
-    const response = handleAdventure(msg.author, msg.content);
-    msg.channel.send(`${msg.author}: ${response}`);
-  }
+  const response = adventureGame.go(msg.author.id, command);
+  msg.channel.send(`${msg.author}: ${response}`);
 });
 
 //make sure this line is the last line
